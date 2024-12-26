a23503cd904ec01be719c5b224ae087f1423d4d2
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-12-26 11:04:17 -0300

    feat: prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { ShortenerModule } from './shortener/shortener.module';
 
 @Module({
   imports: [ShortenerModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaClient],
 })
 export class AppModule {}
