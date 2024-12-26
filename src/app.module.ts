107eb03ffc1c72f430afbd28627d761d89cd6bb7
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-12-26 15:45:19 -0300

    generate user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ShortenerModule } from './shortener/shortener.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ShortenerModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ShortenerModule, ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
   controllers: [],
   providers: [],
   exports: [],
