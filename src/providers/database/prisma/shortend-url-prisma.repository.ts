ef580d33c8cacfd1e748bc58dc9a88221af31183
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-12-26 11:23:11 -0300

    feat: create shortened url repository method

--- src/providers/database/prisma/shortend-url-prisma.repository.ts
+++ src/providers/database/prisma/shortend-url-prisma.repository.ts
@@ -1,12 +1,19 @@
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../../prisma/prisma-client';
 import { ShortenedUrl } from '../../../shortener/entities/shortened-url.entity';
 import { IShortenedUrlRepository } from '../repositories/shortened-url.repository';
+import { CreateShortenedUrlDto } from './dto/create-shortened-url.dto';
 
+@Injectable()
 export class ShortenedUrlPrismaRepository implements IShortenedUrlRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  createShortenedUrl(): Promise<ShortenedUrl> {
-    throw new Error('Method not implemented.');
+  createShortenedUrl(data: CreateShortenedUrlDto): Promise<ShortenedUrl> {
+    const shortenedUrl = this.prisma.shortenedUrl.create({
+      data,
+    });
+
+    return shortenedUrl;
   }
 
   async findShortenedUrlById(id: number): Promise<ShortenedUrl> {
--- src/providers/database/repositories/shortened-url.repository.ts
+++ src/providers/database/repositories/shortened-url.repository.ts
@@ -1,6 +1,7 @@
 import { ShortenedUrl } from '../../../shortener/entities/shortened-url.entity';
+import { CreateShortenedUrlDto } from '../prisma/dto/create-shortened-url.dto';
 
 export interface IShortenedUrlRepository {
-  createShortenedUrl(): Promise<ShortenedUrl>;
+  createShortenedUrl(data: CreateShortenedUrlDto): Promise<ShortenedUrl>;
   findShortenedUrlById(id: number): Promise<ShortenedUrl | null>;
 }
