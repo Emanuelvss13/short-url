5f7d22211a65a70fafc704b5a510d8a223f034b1
Emanuelvss13 <emanuelsouza2002@gmail.com>
2024-12-26 13:59:06 -0300

    feat: validate create shortened url input

--- src/shortener/dto/create-shortener.dto.ts
+++ src/shortener/dto/create-shortener.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty, ApiSchema } from '@nestjs/swagger';
+import { IsUrl } from 'class-validator';
 
 @ApiSchema({ name: 'CreateShortenerRequest' })
 export class CreateShortenerRequest {
   @ApiProperty()
+  @IsUrl()
   sourceUrl: string;
 }
